a6780b8e5757fb3ba0ccaba394f750ffcc08f4a5
petri1807 <petri.lindholm1986@gmail.com>
2021-04-09 16:47:15 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const Person = require('./models/person');
 
 app.use(express.json());
 app.use(cors());
@@ -12,104 +14,98 @@ app.use(
 
 morgan.token('person', (req, res) => JSON.stringify(req.body));
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    number: '040-123456',
-    id: 1,
-  },
-  {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: 2,
-  },
-  {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: 3,
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: 4,
-  },
-];
-
-const generateId = () => {
-  // const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  const num = Math.floor(Math.random() * 10000);
-  return num;
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  next(error);
 };
 
 // MAIN PAGE
 app.get('/', (req, res) => {
   res.send(
-    '<h1>Puhelinluettelo REST API created with Node.js and Express</h1>'
+    '<h1>Puhelinluettelo REST API created with Node.js, Express and MongoDB</h1>'
   );
 });
 
 // PERSONS
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({}).then((people) => {
+    res.json(people);
+  });
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-
-  if (person) res.json(person);
-  else res.status(404).end();
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) res.json(person);
+      else res.status(404).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post('/api/persons', (req, res) => {
   const body = req.body;
 
-  // Missing name
   if (!body.name) {
     return res.status(400).json({
       error: 'name missing',
     });
   }
 
-  // Missing number
   if (!body.number) {
     return res.status(400).json({
       error: 'number missing',
     });
   }
 
-  // Contact already in phonebook
-  if (persons.find((p) => p.name === body.name)) {
-    return res.status(400).json({
-      error: 'name must be unique',
-    });
-  }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save().then((savedPerson) => {
+    res.json(savedPerson);
+  });
+});
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => res.status(204).end())
+    .catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body;
 
   const person = {
     name: body.name,
     number: body.number,
-    id: generateId(),
   };
 
-  persons = [...persons, person];
-
-  res.json(person);
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((p) => p.id !== id);
-
-  res.status(204).end();
-});
-
-// INFO
 app.get('/api/info', (req, res) => {
-  const message = `<p>Phonebook has information for ${
-    persons.length
-  } people</p><br><p>${new Date()}</p>`;
-  res.send(message);
+  Person.countDocuments({})
+    .then((count) => {
+      res.send(
+        `<p>Phonebook has information for ${count} people</p>\n<p>${new Date()}</p>`
+      );
+    })
+    .catch((error) => console.log(error));
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+app.use(errorHandler);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
